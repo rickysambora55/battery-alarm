90c22e557ca25e2543a6efe16dc0697e44a915c5
Ricky <rickysambora55@gmail.com>
2025-02-10 08:12:38 +0700

    update eslint

--- src/main/index.js
+++ src/main/index.js
@@ -1,48 +1,48 @@
-import { app, shell, BrowserWindow, Menu, nativeImage, Tray } from 'electron'
-import { join } from 'path'
-import { electronApp, optimizer, is } from '@electron-toolkit/utils'
-import icon from '../../resources/icon.png?asset'
+import { app, shell, BrowserWindow, Menu, nativeImage, Tray } from "electron";
+import { join } from "path";
+import { electronApp, optimizer, is } from "@electron-toolkit/utils";
+import icon from "../../resources/icon.png?asset";
 
-let mainWindow = null
-let tray = null
+let mainWindow = null;
+let tray = null;
 function createTray() {
-  const icon = join(__dirname, '../../resources/icon.png') // required.
-  const trayicon = nativeImage.createFromPath(icon)
-  tray = new Tray(trayicon.resize({ width: 16 }))
+  const icon = join(__dirname, "../../resources/icon.png"); // required.
+  const trayicon = nativeImage.createFromPath(icon);
+  tray = new Tray(trayicon.resize({ width: 16 }));
   const contextMenu = Menu.buildFromTemplate([
     {
-      label: 'Show App',
+      label: "Show App",
       click: () => {
         if (mainWindow) {
-          mainWindow.show()
-          mainWindow.setSkipTaskbar(false)
+          mainWindow.show();
+          mainWindow.setSkipTaskbar(false);
         } else {
-          createWindow()
+          createWindow();
         }
-      }
+      },
     },
     {
-      label: 'Quit',
+      label: "Quit",
       click: () => {
         if (tray) {
-          tray.destroy()
-          tray = null
+          tray.destroy();
+          tray = null;
         }
-        app.quit()
+        app.quit();
 
-        process.exit(0)
-      }
-    }
-  ])
+        process.exit(0);
+      },
+    },
+  ]);
 
-  tray.setContextMenu(contextMenu)
+  tray.setContextMenu(contextMenu);
 
-  tray.on('click', () => {
+  tray.on("click", () => {
     if (mainWindow) {
-      mainWindow.show()
-      mainWindow.setSkipTaskbar(false)
+      mainWindow.show();
+      mainWindow.setSkipTaskbar(false);
     }
-  })
+  });
 }
 
 function createWindow() {
@@ -52,37 +52,37 @@ function createWindow() {
     height: 670,
     show: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    ...(process.platform === "linux" ? { icon } : {}),
     webPreferences: {
-      preload: join(__dirname, '../preload/index.js'),
-      sandbox: false
-    }
-  })
+      preload: join(__dirname, "../preload/index.js"),
+      sandbox: false,
+    },
+  });
 
-  mainWindow.on('ready-to-show', () => {
-    mainWindow.show()
-  })
+  mainWindow.on("ready-to-show", () => {
+    mainWindow.show();
+  });
 
-  mainWindow.on('close', (event) => {
-    event.preventDefault()
-    mainWindow.hide()
-  })
+  mainWindow.on("close", (event) => {
+    event.preventDefault();
+    mainWindow.hide();
+  });
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
-    shell.openExternal(details.url)
-    return { action: 'deny' }
-  })
+    shell.openExternal(details.url);
+    return { action: "deny" };
+  });
 
   // HMR for renderer base on electron-vite cli.
   // Load the remote URL for development or the local html file for production.
-  if (is.dev && process.env['ELECTRON_RENDERER_URL']) {
-    mainWindow.loadURL(process.env['ELECTRON_RENDERER_URL'])
+  if (is.dev && process.env["ELECTRON_RENDERER_URL"]) {
+    mainWindow.loadURL(process.env["ELECTRON_RENDERER_URL"]);
   } else {
-    mainWindow.loadFile(join(__dirname, '../renderer/index.html'))
+    mainWindow.loadFile(join(__dirname, "../renderer/index.html"));
   }
 
   if (!tray) {
-    createTray()
+    createTray();
   }
 }
 
@@ -91,38 +91,38 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron')
+  electronApp.setAppUserModelId("com.electron");
 
   // Default open or close DevTools by F12 in development
   // and ignore CommandOrControl + R in production.
   // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window)
-  })
+  app.on("browser-window-created", (_, window) => {
+    optimizer.watchWindowShortcuts(window);
+  });
 
-  createWindow()
+  createWindow();
 
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
-app.on('before-quit', () => {
-  app.removeAllListeners()
-})
+app.on("before-quit", () => {
+  app.removeAllListeners();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform === 'darwin') {
-    app.hide()
+app.on("window-all-closed", () => {
+  if (process.platform === "darwin") {
+    app.hide();
   } else {
-    if (mainWindow) mainWindow.hide()
+    if (mainWindow) mainWindow.hide();
   }
-})
+});
 
 // In this file you can include the rest of your app"s specific main process
 // code. You can also put them in separate files and require them here.
